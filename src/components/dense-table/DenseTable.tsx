4bae0939786a53742a6ab1ba73ef4d1c5a5ee5b7
chirangashalitha <chiranga.20244008@iit.ac.lk>
2024-09-01 13:07:55 +0530

    Add table data

--- src/components/dense-table/DenseTable.tsx
+++ src/components/dense-table/DenseTable.tsx
@@ -33,7 +33,6 @@ const columns: readonly Column[] = [
     id: "size",
     label: "Size\u00a0(km\u00b2)",
     minWidth: 170,
-    align: "right",
     format: (value: number) => value.toLocaleString("en-US"),
   },
   {

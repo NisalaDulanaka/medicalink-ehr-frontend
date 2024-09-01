e252f1aaec49260c802f9eb31e6eddf6c292ade5
Nisala Dulanaka <dulanakanisala654@gmail.com>
2024-09-01 13:57:52 +0530

    Finalizing tab system for review

--- src/components/tab-navigation/TabContext.tsx
+++ src/components/tab-navigation/TabContext.tsx
@@ -1,59 +1,38 @@
-import { createContext, Key, useContext, useState } from "react";
-import { HomeTab, PatientTab, SettingsTab } from "./TabNavigator";
-
-export interface ITabProps {
-    key: Key | number,
-    title: String,
-    element: React.ReactNode
-}
-
-export interface ITabContextProps {
-    addTab: (tab: ITabProps) => void;
-    removeTab: (index: number) => void;
-    tabs: ITabProps[];
-    setTabs: (element: ITabProps[]) => void;
-    activeTab: number;
-    setActiveTab: (index: number) => void;
-}
-
-export interface ITabContextProviderProps{
-    children: React.ReactNode;
-}
-
-export const TabContext = createContext<ITabContextProps | undefined>(undefined);
-
-export const useTabContext = () => {
-    const context = useContext<ITabContextProps | undefined>(TabContext);
-
-    if(! context) {
-        throw new Error('useTabContext must be used within a TabProvider');
-    }
-
-    return context;
-}
+import { useState } from "react";
+import { HomeTab } from "./TabNavigator";
+import { ITabContextProviderProps, ITabProps, TabContext } from "./tabUtils";
 
 export default function TabContextProvider({children}: ITabContextProviderProps) {
+    const [tabCount, setTabCount] = useState<number>(2);
     const [tabs, setTabs] = useState<ITabProps[]>([
         { key: 1, title: 'Home', element: <HomeTab /> },
-        { key: 2, title: 'Seneli 20 yrs, Female, Radiology', element: <PatientTab /> },
-        { key: 3, title: 'Settings', element: <SettingsTab /> },
-        { key: 4, title: 'Counter Tab', element: <HomeTab /> }
     ]);
     const [activeTab, setActiveTab] = useState(0);
 
     const removeTab = (index: number) => {
         // Remove the tab and create a new list
-        let newTabs = tabs.slice(0,index).concat(tabs.slice(index + 1, tabs.length));
-
+        const newTabs = tabs.filter((tab, i) => index !== i);
+        
         setTabs(newTabs);
+        if(index == newTabs.length) {
+            setActiveTab(0);
+        }
+        else if(activeTab != 0) setActiveTab(activeTab - 1);
     }
 
-    const addTab = (tab: ITabProps) => {
-        // Remove the tab and create a new list
-        let newTabs = [...tabs];
-        newTabs.push(tab);
+    const addTab = (
+        tab: {
+            title: string,
+            element: React.ReactNode
+        }
+    ) => {
+        // create new list and add the tab
+        const newTabs = [...tabs];
+        newTabs.push({...tab, key: tabCount});
 
         setTabs(newTabs);
+        setTabCount(tabCount + 1);
+        setActiveTab(newTabs.length - 1);
     }
 
     return (

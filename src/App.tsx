81a7cb055257e1dd8e2ad0e80250268c61134027
chirangashalitha <chiranga.20244008@iit.ac.lk>
2024-09-01 14:49:28 +0530

    Added patient information card w/o action buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import AppHeader from "./components/app-header/AppHeader";
+import PatientInfo from "./components/patient-info/PatientInfo";
 import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
@@ -8,23 +9,27 @@ function App() {
   return (
     <>
       <div className="h-[100vh] flex flex-col">
-
         <div className="flex-initial">
-          <AppHeader /> {/* This is where you put the appbar. I just created a component named AppHeader change it as you like */}
+          <AppHeader />{" "}
+          {/* This is where you put the appbar. I just created a component named AppHeader change it as you like */}
         </div>
 
         <div className="flex-auto flex">
-
           <div className="flex-initial">
             <Sidebar /> {/* This is the sidebar */}
           </div>
 
           <div className="flex-auto bg-green-100">
             {/* This is where I'll add the tab component */}
+            <PatientInfo
+              name="Seneli Jayasinghe"
+              dob="19/06/2004"
+              identifier="200456456456"
+              gender="F"
+              address="243 2nd Cross Street, Colombo"
+            />
           </div>
-
         </div>
-
       </div>
     </>
   );
---
+++ src/components/patient-info/PatientInfo.tsx
@@ -0,0 +1,46 @@
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Seneli from "../../assets/images/Seneli.jpg";
+import { Box } from "@mui/material";
+
+interface Props {
+  name: string;
+  identifier: string;
+  dob: string;
+  gender: string;
+  address: string;
+}
+
+function PatientInfo(props: Props) {
+  return (
+    <Card
+      sx={{
+        maxWidth: 800,
+        maxHeight: "auto",
+        margin: 1,
+        display: "flex",
+        flexDirection: "row",
+      }}
+    >
+      <Box sx={{ height: 128, width: 128, margin: 1 }}>
+        <img src={Seneli} />
+      </Box>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {props.name}
+        </Typography>
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+          <p>Identifier: {props.identifier}</p>
+          <p>Date of Birth: {props.dob}</p>
+          <p>Gender: {props.gender}</p>
+          <p>Address: {props.address}</p>
+        </Typography>
+      </CardContent>
+      <CardActions></CardActions>
+    </Card>
+  );
+}
+
+export default PatientInfo;
